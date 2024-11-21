fed558b503ab3545a819394f1269e2bd12b567d9
Alexander Belovitsky <mirrexone@gmail.com>
2024-11-21 14:28:56 +0300

    Add spring context and annotation api

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework:spring-core:6.2.0")
+    implementation("org.springframework:spring-context:6.2.0")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
